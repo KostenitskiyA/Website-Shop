4c3aa5a3f0545106a89aff1cfd75853361645292
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-07-28 21:58:46 +0200

    Добавлен класс Profile

---
+++ Website-Shop/Models/Profile.cs
@@ -0,0 +1,35 @@
+﻿namespace Website_Shop.Models
+{
+    public class Profile
+    {
+        /// <summary>
+        /// Идентификатор профиля
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Имя профиля
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Ссылка на пользователя
+        /// </summary>
+        public User User { get; set; }
+
+        /// <summary>
+        /// Идентификатор типа пользователя
+        /// </summary>
+        public int UserTypeId { get; set; }
+
+        /// <summary>
+        /// Ссылка на тип пользователя
+        /// </summary>
+        public UserType UserType { get; set; }
+    }
+}
