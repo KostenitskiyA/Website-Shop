58e3ff5df6368f34e8cf4d9f294a3c8ff0c428e9
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-07-28 21:58:57 +0200

    Добавлен класс User

---
+++ Website-Shop/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace Website_Shop.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Ссылка на профиль
+        /// </summary>
+        public Profile Profile { get; set; }
+    }
+}
