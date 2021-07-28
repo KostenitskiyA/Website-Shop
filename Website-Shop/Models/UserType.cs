ea251b5c0a5cd88921eab030dd0b258d6dc4f15c
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-07-28 21:59:09 +0200

    Добавлен класс UserType

---
+++ Website-Shop/Models/UserType.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Website_Shop.Models
+{
+    public class UserType
+    {
+        /// <summary>
+        /// Идентификатор типа
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Имя типа
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Коллекция профилей
+        /// </summary>
+        public List<Profile> Profiles { get; set; }
+    }
+}
