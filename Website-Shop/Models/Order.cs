1367677bc808a7400c878ddc9a5be6c76bc207cd
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-08-02 01:39:31 +0200

    Добавлен класс Order

---
+++ Website-Shop/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Website_Shop.Models
+{
+    /// <summary>
+    /// Заказ
+    /// </summary>
+    public class Order
+    {
+        /// <summary>
+        /// Идентификатор заказа
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Идентификатор профиля
+        /// </summary>
+        public int ProfileId { get; set; }
+
+        /// <summary>
+        /// Ссылка на профиль
+        /// </summary>
+        public Profile Profile { get; set; }
+
+        /// <summary>
+        /// Коллекция продуктов
+        /// </summary>
+        public List<OrderItem> Items { get; set; }
+    }
+}
