b1c8417fa35dc93bc7658db3e519fb1307e63cec
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-08-02 01:39:48 +0200

    Добавлен класс BasketItem

---
+++ Website-Shop/Models/BasketItem.cs
@@ -0,0 +1,38 @@
+﻿namespace Website_Shop.Models
+{
+    /// <summary>
+    /// Элемент корзины
+    /// </summary>
+    public class BasketItem
+    {
+        /// <summary>
+        /// Идентификатор элемента корзины
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Количество продуктов
+        /// </summary>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Идентификатор корзины
+        /// </summary>
+        public int BasketId { get; set; }
+
+        /// <summary>
+        /// Ссылка на корзину
+        /// </summary>
+        public Basket Basket { get; set; }
+
+        /// <summary>
+        /// Идентификатор продукта
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Ссылка на продукт
+        /// </summary>
+        public Product Product { get; set; }
+    }
+}
