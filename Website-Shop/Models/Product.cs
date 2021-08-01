ff8b77ec18600b23da3e7114796aba4c56c4e035
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-08-02 01:49:11 +0200

    Добавлен класс Product

---
+++ Website-Shop/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace Website_Shop.Models
+{
+    /// <summary>
+    /// Продукт
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Идентификатор продукта
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название продукта
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание продукта
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
