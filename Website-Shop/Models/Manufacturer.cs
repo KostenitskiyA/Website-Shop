adbca1e8f9e2b2971934ecc07652e452247167f5
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-08-02 23:13:51 +0200

    Добавлен класс Manufacturer

---
+++ Website-Shop/Models/Manufacturer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Website_Shop.Models
+{
+    /// <summary>
+    /// Производитель
+    /// </summary>
+    public class Manufacturer
+    {
+        /// <summary>
+        /// Идентификатор производителя
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование производителя
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Коллекция продуктов
+        /// </summary>
+        public List<Product> Products { get; set; }
+    }
+}
