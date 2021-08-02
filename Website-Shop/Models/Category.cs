8692dc5548d4b3209ad4e1a56ae9f56b33d49a75
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-08-02 23:13:31 +0200

    Добавлен класс Category

---
+++ Website-Shop/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Website_Shop.Models
+{
+    /// <summary>
+    /// Категория
+    /// </summary>
+    public class Category
+    {
+        /// <summary>
+        /// Идентификатор категории
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Наименование категории
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Коллекция продуктов
+        /// </summary>
+        public List<Product> Products { get; set; } 
+    }
+}
