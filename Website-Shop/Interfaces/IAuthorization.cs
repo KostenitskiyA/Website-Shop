f0c97a1668f823f146c2966174104bfed761fa21
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-08-09 01:57:34 +0200

    Добавлен интерфейс IAuthorization

---
+++ Website-Shop/Interfaces/IAuthorization.cs
@@ -0,0 +1,21 @@
+﻿using Website_Shop.Models.Entities;
+
+namespace Website_Shop.Interfaces
+{
+    public interface IAuthorization
+    {
+        /// <summary>
+        /// Авторизация пользователя
+        /// </summary>
+        /// <param name="user">Пользователь для авторизации</param>
+        /// <returns>Авторизированный пользователь</returns>
+        public User LogIn(User user);
+
+        /// <summary>
+        /// Регистрация пользователя
+        /// </summary>
+        /// <param name="user">Пользователь для регистрации</param>
+        /// <returns>Авторизированный пользователь</returns>
+        public User SignIn(User user);
+    }
+}
