9bf95ff7c299cb2750e6939949d6e430b9b9f708
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-08-19 19:17:59 +0200

    Добавлен контроллер AuthorizationController

---
+++ Website-Shop/Controllers/AuthorizationController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Website_Shop.Interfaces;
+using Website_Shop.Models;
+using Website_Shop.Models.Entities;
+
+namespace Website_Shop.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IAuthorizationProvider _authorization;
+
+        public AuthorizationController(ILogger<HomeController> logger, IAuthorizationProvider authorization)
+        {
+            _logger = logger;
+            _authorization = authorization;
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(User user)
+        {
+            try
+            {
+                var foundUser = await _authorization.LogInAsync(user);
+
+                if (foundUser == null)
+                    throw new Exception("Пользователь не найден");
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(Authorization(user.Authorization.Login, user.Role.ToString())));
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.StackTrace);
+
+                ModelState.AddModelError("", ex.Message);
+
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(User user)
+        {
+            try
+            {
+                var createdUser = await _authorization.SignInAsync(user);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(Authorization(createdUser.Authorization.Login, createdUser.Role.ToString())));
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.StackTrace);
+
+                ModelState.AddModelError("", ex.Message);
+
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult LogOut()
+        {
+            try
+            {
+                HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.StackTrace);
+
+                return RedirectToAction("Index", "Error", new Error()
+                {
+                    Text = ex.Message
+                });
+            }
+        }
+
+        private ClaimsIdentity Authorization(string login, string role)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, login),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, role)
+            };
+
+            return new ClaimsIdentity(claims,
+                "ApplicationCookie",
+                ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+        }
+    }
+}
