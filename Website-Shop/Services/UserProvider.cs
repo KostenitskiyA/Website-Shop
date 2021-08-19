17484e197fc7dc92e392f04d3cea8871d749b088
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-08-20 01:47:23 +0200

    Добавлен класс UserProvider

---
+++ Website-Shop/Services/UserProvider.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Website_Shop.Interfaces;
+using Website_Shop.Models;
+using Website_Shop.Models.Entities;
+
+namespace Website_Shop.Services
+{
+    public class UserProvider : IUserProvider
+    {
+        private ApplicationContext _context;
+
+        public UserProvider(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUserAsync(int userId)
+        {
+            try
+            {
+                var user = await _context.Users
+                    .Include(u => u.Authorization)
+                    .Include(u => u.Profile)
+                    .SingleOrDefaultAsync(u => u.Id == userId);
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            try
+            {
+                var users = await _context.Users
+                    .Include(u => u.Authorization)
+                    .Include(u => u.Profile)
+                    .ToListAsync();
+
+                return users;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task EditUserAsync(User user)
+        {
+            try
+            {
+                _context.Entry<User>(user).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public async Task DeleteUserAsync(int userId)
+        {
+            try
+            {
+                var user = _context.Users.SingleOrDefault(u => u.Id == userId);
+
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
