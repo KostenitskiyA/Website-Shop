b4355971d78e1b0dd730a8b410592f483d490f53
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-08-19 19:16:23 +0200

    Переименован интерфейс IAuthorization в IAuthorizationProvider. Изменены методы интерфейса. Добавлены комментарии

--- Website-Shop/Interfaces/IAuthorizationProvider.cs
+++ Website-Shop/Interfaces/IAuthorizationProvider.cs
@@ -1,21 +1,25 @@
-﻿using Website_Shop.Models.Entities;
+﻿using System.Threading.Tasks;
+using Website_Shop.Models.Entities;
 
 namespace Website_Shop.Interfaces
 {
-    public interface IAuthorization
+    /// <summary>
+    /// Провайдер действий с авторизацией
+    /// </summary>
+    public interface IAuthorizationProvider
     {
         /// <summary>
         /// Авторизация пользователя
         /// </summary>
         /// <param name="user">Пользователь для авторизации</param>
         /// <returns>Авторизированный пользователь</returns>
-        public User LogIn(User user);
+        public Task<User> LogInAsync(User user);
 
         /// <summary>
         /// Регистрация пользователя
         /// </summary>
         /// <param name="user">Пользователь для регистрации</param>
         /// <returns>Авторизированный пользователь</returns>
-        public User SignIn(User user);
+        public Task<User> SignInAsync(User user);
     }
 }
