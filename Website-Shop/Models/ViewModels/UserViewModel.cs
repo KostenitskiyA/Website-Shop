c4b10d2621b9f5f0400cdf671a8dbb51a95bd277
kostenitskiy.alexander.work@gmail.com <kostenitskiy.alexander.work@gmail.com>
2021-09-10 02:12:55 +0200

    Добавлены модели UserViewModel, UsersViewModel, EditUserViewModel

---
+++ Website-Shop/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Website_Shop.Models.Entities;
+
+namespace Website_Shop.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+    }
+}
---
+++ Website-Shop/Models/ViewModels/UsersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Website_Shop.Models.Entities;
+
+namespace Website_Shop.Models.ViewModels
+{
+    public class UsersViewModel
+    {
+        public IEnumerable<User> Users { get; set; }
+    }
+}
